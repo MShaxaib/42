a7b338134580a6bef5064e241b55c9bc5eeb85c3
MShaxaib <139437420+MShaxaib@users.noreply.github.com>
2023-07-15 23:31:06 +0400

    Add files via upload

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -6,34 +6,37 @@
 /*   By: mshazaib <mshazaib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 21:02:34 by mshazaib          #+#    #+#             */
-/*   Updated: 2023/07/11 19:51:06 by mshazaib         ###   ########.fr       */
+/*   Updated: 2023/07/14 21:37:17 by mshazaib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t n)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t i;
-	size_t j;
+	size_t	j;
+	size_t	dstlen;
+	size_t	srclen;
 
-	j = 0;
-	
-	i = ft_strlen(dst);
+	dstlen = strlen(dst);
+	srclen = strlen((char *)src);
+	if (dstsize < dstlen)
+		return (srclen + dstsize);
+	while (dstlen + 1 < dstsize && src[j] != 0)
+		dst[dstlen++] = src[j++];
+	if (dstlen < dstsize)
+		dst[dstlen] = '\0';
+	while (src[j++] != 0)
+		dstlen++;
+	return (dstlen);
+}
 
-	while(*dst)
-	{
-		src[j] + dst[i];
-		
-	}
-	return(0);
-}
-int main()
-{
-	char src[] = "Hello";
-	char dst[] = "world";
-	
-	printf("%zu/n", ft_strlcat(dst, src, 1));
-	printf("%zu/n", ft_strlcat(dst, src, 1));
-		
-}
+// int	main()
+// {
+// 	char dst[10] = "salam";
+// 	char src[] = "sha";
+// 	size_t dstsize = 10;
+
+// 	printf("%zu\n", ft_strlcat(dst, src, dstsize));
+// 	// printf("%zu", strlcat(dst, src, dstsize));
+// }
